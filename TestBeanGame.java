38220eede8a34f5151f5b463400ef136cd65aebd
LiaoKunZhi <a0956823675@gmail.com>
2016-03-20 21:36:42 +0800

    Create TestBeanGame.java
    Make a main method to call the game from the BeanGame.java.

---
+++ TestBeanGame.java
@@ -0,0 +1,13 @@
+//U10416043   廖崑智
+
+import javafx.application.Application;
+
+//Class
+public class TestBeanGame {
+
+	//Main method
+	public static void main(String[] args) {
+		//Call the BeanGame class
+		Application.launch(BeanGame.class,args);
+	}
+}
