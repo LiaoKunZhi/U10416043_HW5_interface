a2640e22918545ca28a407c5b88786b08a827529
LiaoKunZhi <a0956823675@gmail.com>
2016-03-20 21:42:58 +0800

    Create BeanGame.java
    Create a new class to carry the game.Make a new constructor and override the call.

---
+++ BeanGame.java
@@ -0,0 +1,19 @@
+//U1049643   廖崑智
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+
+//Class
+public class BeanGame extends Application {
+
+	//Create a no-arg constructor
+	public BeanGame () {
+	}
+
+	@Override
+}
